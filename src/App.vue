4fac8ca7cda32fbac81ede61e1a9d77dc49360a4
Mathis S.M <contact@minarox.fr>
2023-05-26 11:12:39 +0200

    Add Heading component

--- src/App.vue
+++ src/App.vue
@@ -19,18 +19,21 @@
     />
   </article>-->
 
+  <Heading @bright="setBright" @hot="setHot" @near="setNear" />
   <Map />
 </template>
 
 <script>
 import dataset from "@/assets/json/stars.json";
 //import Table from "@/components/Table.vue";
+import Heading from "@/components/Heading.vue";
 import Map from "@/components/Map.vue";
 
 export default {
   name: "App",
   components: {
     //Table,
+    Heading,
     Map,
   },
   data() {
@@ -39,6 +42,9 @@ export default {
       near_stars: undefined,
       bright_stars: undefined,
       hot_stars: undefined,
+      bright_nb: null,
+      hot_nb: null,
+      near_nb: null,
     };
   },
   mounted() {
@@ -56,6 +62,15 @@ export default {
         )
         .slice();
     },
+    setBright(value) {
+      this.bright_nb = parseInt(value);
+    },
+    setHot(value) {
+      this.hot_nb = parseInt(value);
+    },
+    setNear(value) {
+      this.near_nb = parseInt(value);
+    },
   },
 };
 </script>
@@ -66,9 +81,9 @@ export default {
 }
 
 ::-webkit-scrollbar-thumb {
-  box-shadow: inset 0 0 14px 14px black;
   border: solid 4px transparent;
   border-radius: 14px;
+  box-shadow: inset 0 0 14px 14px black;
 }
 
 ::-webkit-scrollbar-button {
@@ -76,24 +91,24 @@ export default {
 }
 
 * {
+  box-sizing: border-box;
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
 }
 
 body {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  scroll-behavior: smooth;
   color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   overflow: overlay;
+  scroll-behavior: smooth;
 }
 
 article {
-  margin: 1.4rem 1.4rem 2rem;
   display: flex;
   flex-direction: column;
   gap: 2.4rem;
+  margin: 1.4rem 1.4rem 2rem;
 }
 </style>
---
+++ src/components/Heading.vue
@@ -0,0 +1,111 @@
+<template>
+  <header>
+    <section>
+      <form @submit.prevent>
+        <label for="name">Rechercher une étoile : </label>
+        <input id="name" name="name" type="text" />
+      </form>
+    </section>
+    <section>
+      <form @submit.prevent>
+        <div>
+          <label for="near_nb">Étoiles distantes : </label>
+          <input id="near_nb" v-model="near_nb" name="near_nb" type="range" />
+          <span>{{ near_nb }}</span>
+        </div>
+        <div>
+          <label for="bright_nb">Étoiles brillantes : </label>
+          <input
+            id="bright_nb"
+            v-model="bright_nb"
+            name="bright_nb"
+            type="range"
+          />
+          <span>{{ bright_nb }}</span>
+        </div>
+        <div>
+          <label for="hot_nb">Étoiles chaudes : </label>
+          <input id="hot_nb" v-model="hot_nb" name="hot_nb" type="range" />
+          <span>{{ hot_nb }}</span>
+        </div>
+      </form>
+    </section>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeadingComponent",
+  data() {
+    return {
+      near_nb: 0,
+      bright_nb: 0,
+      hot_nb: 0,
+    };
+  },
+  watch: {
+    near_nb(value) {
+      this.$emit("near", value);
+    },
+    bright_nb(value) {
+      this.$emit("bright", value);
+    },
+    hot_nb(value) {
+      this.$emit("hot", value);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 1rem;
+  justify-content: space-between;
+  left: 0;
+  padding: 1rem;
+  pointer-events: all;
+  position: absolute;
+  right: 0;
+  top: 0;
+  z-index: 1;
+
+  > section {
+    > form {
+      align-items: center;
+      display: flex;
+      flex-flow: row nowrap;
+      gap: 1rem;
+      justify-content: flex-start;
+
+      input[type="text"] {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        outline: none;
+        padding: 0.5rem;
+      }
+
+      > div {
+        align-items: center;
+        display: flex;
+        flex-flow: row nowrap;
+        gap: 0.4rem;
+        justify-content: flex-start;
+
+        span {
+          text-align: center;
+          width: 20px;
+        }
+      }
+    }
+
+    &:last-of-type > form {
+      gap: 2rem;
+    }
+  }
+}
+</style>
